8519432f7d618df33f379c2fe222602eb6cf6d58
Germán Cáceres <99841917+Gpcaceres@users.noreply.github.com>
2025-08-24 14:56:53 -0500

    Add Postman collection for login testing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login.component';
+import { CallbackComponent } from './pages/callback.component';
 import { PedidoFormComponent } from './components/pedido-form.component';
 import { PedidoListComponent } from './components/pedido-list.component';
 import { TrackingComponent } from './components/tracking.component';
@@ -9,6 +10,7 @@ import { RoleGuard } from './guards/role.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'callback', component: CallbackComponent },
   {
     path: 'nuevo',
     component: PedidoFormComponent,

fe6837880b9bb1b363a72d9d4fcb8cd645041d01
Germán Cáceres <99841917+Gpcaceres@users.noreply.github.com>
2025-08-24 00:05:52 -0500

    Remove password from ClienteRes and hide in Cliente entity

--- back-end/cliente-service/src/main/java/com/example/cliente_service/clientes/dto/ClienteRes.java
+++ back-end/cliente-service/src/main/java/com/example/cliente_service/clientes/dto/ClienteRes.java
@@ -2,8 +2,8 @@ package com.example.cliente_service.clientes.dto;
 import com.example.cliente_service.clientes.Cliente;
 import java.util.UUID;
 
-public record ClienteRes(UUID id, String nombre, String email, String telefono, String clave) {
+public record ClienteRes(UUID id, String nombre, String email, String telefono) {
   public static ClienteRes of(Cliente c) {
-    return new ClienteRes(c.getId(), c.getNombre(), c.getEmail(), c.getTelefono(), c.getClave());
+    return new ClienteRes(c.getId(), c.getNombre(), c.getEmail(), c.getTelefono());
   }
 }
